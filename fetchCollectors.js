079c1bc1e1f6533e625fec346324af9ead761261
RedVeil <leon.niesler@gmail.com>
2024-06-25 18:54:35 +0200

    added pages batch1

--- fetchCollectors.js
+++ fetchCollectors.js
@@ -1,61 +1,68 @@
 import puppeteer from "puppeteer";
 import fs from "fs";
-import cheerio from "cheerio";
+import cheerio from "cheerio"
+import collectors from './filteredResults.json' assert { type: 'json' };
+
 
 const wait = (milliseconds) => {
   return new Promise(resolve => setTimeout(resolve, milliseconds));
 };
 
 (async () => {
-  const browser = await puppeteer.launch({
-    headless: false,  // Set to true if you want to run in headless mode
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  });
-  const page = await browser.newPage();
+  if (collectors.length > 0) {
+    const browser = await puppeteer.launch({
+      headless: false,  // Set to true if you want to run in headless mode
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    const page = await browser.newPage();
 
-  // Open the login page
-  await page.goto('https://www.larryslist.com/', {
-    waitUntil: 'networkidle2'
-  });
+    // Open the login page
+    await page.goto('https://www.larryslist.com/', {
+      waitUntil: 'networkidle2'
+    });
 
-  // Click the login button to open the login form (assuming there's a button to click to show the form)
-  await page.click('span[class="login"]'); // Modify this selector to match the actual login button/link
+    // Click the login button to open the login form (assuming there's a button to click to show the form)
+    await page.click('span[class="login"]'); // Modify this selector to match the actual login button/link
 
-  // Wait for the login form to be displayed
-  await page.waitForSelector('form#login'); // Modify this selector to match the actual login form
+    // Wait for the login form to be displayed
+    await page.waitForSelector('form#login'); // Modify this selector to match the actual login form
 
-  // Type the email and password
-  await page.type('input[id="email"]', 'leon.niesler@gmail.com'); // Replace with your actual email
-  await page.type('input[id="password"]', 'leon1996'); // Replace with your actual password
+    // Type the email and password
+    await page.type('input[id="email"]', 'leon.niesler@gmail.com'); // Replace with your actual email
+    await page.type('input[id="password"]', 'leon1996'); // Replace with your actual password
 
-  // Submit the login form
-  await page.click('button[type="submit"]'); // Modify this selector to match the actual submit button
+    // Submit the login form
+    await page.click('button[type="submit"]'); // Modify this selector to match the actual submit button
 
-  wait(10_000)
+    wait(10_000)
 
-  await page.goto(`https://www.larryslist.com/collector/name/Ann%20Tenenbaum/2003/`, {
-    waitUntil: 'networkidle2'
-  });
+    for (let n = 608; n < collectors.length; n++) {
+      await page.goto(`https://www.larryslist.com/collector/name/${collectors[n].label}/${collectors[n].value}/`, {
+        waitUntil: 'networkidle2'
+      });
 
-  const bodyHTML = await page.evaluate(() => {
-    return document.body.innerHTML;
-  });
+      const bodyHTML = await page.evaluate(() => {
+        return document.body.innerHTML;
+      });
 
-  const $ = cheerio.load(bodyHTML);
-  getGenres($)
-  getOrigins($)
-  getMediums($)
+      const $ = cheerio.load(bodyHTML);
+      const body = $('#bodycontainer').html()
+      // getGenres($)
+      // getOrigins($)
+      // getMediums($)
 
-  // Close the browser
-  //await browser.close();
+      fs.writeFile(`./pages/${collectors[n].value}.html`, body, err => {
+        if (err) {
+          console.log('Error writing to file', err);
+        } else {
+          console.log(`Results saved to ./pages/${collectors[n].value}.html`);
+        }
+      });
+    }
 
-  // fs.writeFile('filteredResults.json', JSON.stringify(resultsArray, null, 2), err => {
-  //   if (err) {
-  //     console.log('Error writing to file', err);
-  //   } else {
-  //     console.log('Results saved to filteredResults.json');
-  //   }
-  // });
+    // Close the browser
+    await browser.close();
+  }
 })();
 
 
